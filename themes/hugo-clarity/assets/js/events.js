fcee004c452340732f525765dd7ab553e026d548
adriana long <adriana.michelle.long@gmail.com>
2024-02-07 15:22:34 -0500

    updates

--- themes/hugo-clarity/assets/js/events.js
+++ themes/hugo-clarity/assets/js/events.js
@@ -1,13 +1,13 @@
 //this is for the perks we provide
 
-const sponsorDic =[
+const sponsorDic = [
   {
-    img:"./logos/cyberbae.png",
-    description:"include brief description of sponsor"
+    img: "./logos/cyberbae.png",
+    description: "include brief description of sponsor"
   },
   {
-    img:"./logos/nucamp.png",
-    description:"include brief description of sponsor"
+    img: "./logos/nucamp.png",
+    description: "include brief description of sponsor"
 
   },
 ]
@@ -15,12 +15,12 @@ const sponsorDic =[
 const perkDic = [
   {
     img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiVBiC1of0YoXcsjATc44vEND9abjCaQgtJ5RnA0FDf55yd-7GjSUMKn_5yqQiBtTub5LMVzK1rkjr76vEErhDP8QMXtf6Xc2k2JEmUpPap0lLtY2hRX6O4ZXIbmv-ktHy2heFrJoiTILkuus56-4MiAUE1NZdiRogFiiXk-nyFijIEM60/s1600/14.png",
-    title: "Affordable Courses",
+    title: "Brand Development",
     description: "We provide afordable cyber security courses to help you upskil for your next career pivot"
   },
   {
     img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiB_xHCZZkHCyhBiH0rIkPJJ3IvwLCKabLIXEAjnVJmFewBVC9VgKqG9KZUD6i2UDtZLN06zhRQC4Ic6B8B_UFNJ07PtfewHrgUxTHe3nl0AJUCYG-T4RCNDH3sjW-Jz2aAiGCg5SActMUQ6JMlUTeaJaX4_6vQgKwkp9xVRmwuIPKt_1k/s1600/13.png",
-    title: "Dedicated Network",
+    title: "Logo Creation",
     description: "We're more than an organization. We're a community of motivated professions here to help you advance your career"
 
   },
@@ -36,7 +36,7 @@ const perkDic = [
   {
     img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEghlHptcSdZ1lrVHG07nuPMy55SK8BHqsN7zvAc1B7MI4csYoG5iRhDQW-yehrOlbVNtFXG0BXffkiWZxYs_XdOn5Hc03cdOslh8Q9ZrG7dyqHL1x_pQRv9hdlEiUL5bWX2SvuHkeziK9TZfTD9wu0S55SRB3KzRW4qs3o1K55gkcvEB9c/s1600/Beige%20Business%20Brand%20Color%20Wheel%20Palette%20Facebook%20Post%20(8).png",
     title: "Website Maintanence and Support"
-  }, 
+  },
   {
     img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEghlHptcSdZ1lrVHG07nuPMy55SK8BHqsN7zvAc1B7MI4csYoG5iRhDQW-yehrOlbVNtFXG0BXffkiWZxYs_XdOn5Hc03cdOslh8Q9ZrG7dyqHL1x_pQRv9hdlEiUL5bWX2SvuHkeziK9TZfTD9wu0S55SRB3KzRW4qs3o1K55gkcvEB9c/s1600/Beige%20Business%20Brand%20Color%20Wheel%20Palette%20Facebook%20Post%20(8).png",
     title: "Business Consultancy"
@@ -52,50 +52,55 @@ const perkDic = [
   {
     img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEghlHptcSdZ1lrVHG07nuPMy55SK8BHqsN7zvAc1B7MI4csYoG5iRhDQW-yehrOlbVNtFXG0BXffkiWZxYs_XdOn5Hc03cdOslh8Q9ZrG7dyqHL1x_pQRv9hdlEiUL5bWX2SvuHkeziK9TZfTD9wu0S55SRB3KzRW4qs3o1K55gkcvEB9c/s1600/Beige%20Business%20Brand%20Color%20Wheel%20Palette%20Facebook%20Post%20(8).png",
     title: "Graphic Design"
-  }
+  },
+  {
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEghlHptcSdZ1lrVHG07nuPMy55SK8BHqsN7zvAc1B7MI4csYoG5iRhDQW-yehrOlbVNtFXG0BXffkiWZxYs_XdOn5Hc03cdOslh8Q9ZrG7dyqHL1x_pQRv9hdlEiUL5bWX2SvuHkeziK9TZfTD9wu0S55SRB3KzRW4qs3o1K55gkcvEB9c/s1600/Beige%20Business%20Brand%20Color%20Wheel%20Palette%20Facebook%20Post%20(8).png",
+    title: "Publishing Advertising"
+  },
+
 ];
 
 
 // this is for the events
 const eventDic = [
-    {
-      title: "Unlock Your Job Potential: Build a Resume That Gets You Noticed",
-      description: "Tired of resumes that blend into the pile? This workshop equips you with the tools to build a resume that grabs attention and lands interviews.",
-      date: new Date(2024, 2, 21), // May 27, 2024
-      rsvpLink: "https://example.com/rsvp/coding-workshop",
-      img:"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhE-OXdp-Aah1C_L5hKlk8KQHHSFHNHEZuoQEk9Rb5LEJncc0rgVXeX-AwLP0U7Zr79pIQdDShltnjWQghbojCb0ThjGFsv_J9CFsQc-KsqCQEmrSYf3g-FR0jMKjSrTWXykYIhrOuJNLQjYLK1yeBobKxyMnhZbRKjsaGmpvUX7xc3Xuk/s1600/Beige%20Business%20Brand%20Color%20Wheel%20Palette%20Facebook%20Post%20(7).png"
-    },
-    {
-      title: "Land Your Dream Job: A Masterclass in Job Hunting Success",
-      description: "This immersive masterclass equips you with the winning strategies to navigate the modern job market and land the role you deserve.",
-      date: new Date(2024, 2, 28), // May 27, 2024
-      rsvpLink: "https://example.com/rsvp/coding-workshop",
-      img:"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhE-OXdp-Aah1C_L5hKlk8KQHHSFHNHEZuoQEk9Rb5LEJncc0rgVXeX-AwLP0U7Zr79pIQdDShltnjWQghbojCb0ThjGFsv_J9CFsQc-KsqCQEmrSYf3g-FR0jMKjSrTWXykYIhrOuJNLQjYLK1yeBobKxyMnhZbRKjsaGmpvUX7xc3Xuk/s1600/Beige%20Business%20Brand%20Color%20Wheel%20Palette%20Facebook%20Post%20(7).png"
-    },
-    {
-      title: "Beyond the Chat: Supercharge Your Productivity with ChatGPT",
-      description: "Learn the basics of HTML, CSS, and JavaScript in this interactive workshop.",
-      date: new Date(2024, 3, 6), // May 27, 2024
-      rsvpLink: "https://example.com/rsvp/coding-workshop",
-      img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhE-OXdp-Aah1C_L5hKlk8KQHHSFHNHEZuoQEk9Rb5LEJncc0rgVXeX-AwLP0U7Zr79pIQdDShltnjWQghbojCb0ThjGFsv_J9CFsQc-KsqCQEmrSYf3g-FR0jMKjSrTWXykYIhrOuJNLQjYLK1yeBobKxyMnhZbRKjsaGmpvUX7xc3Xuk/s1600/Beige%20Business%20Brand%20Color%20Wheel%20Palette%20Facebook%20Post%20(7).png"
-    },
-  ];
+  {
+    title: "Unlock Your Job Potential: Build a Resume That Gets You Noticed",
+    description: "Tired of resumes that blend into the pile? This workshop equips you with the tools to build a resume that grabs attention and lands interviews.",
+    date: new Date(2024, 2, 21), // May 27, 2024
+    rsvpLink: "https://example.com/rsvp/coding-workshop",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhE-OXdp-Aah1C_L5hKlk8KQHHSFHNHEZuoQEk9Rb5LEJncc0rgVXeX-AwLP0U7Zr79pIQdDShltnjWQghbojCb0ThjGFsv_J9CFsQc-KsqCQEmrSYf3g-FR0jMKjSrTWXykYIhrOuJNLQjYLK1yeBobKxyMnhZbRKjsaGmpvUX7xc3Xuk/s1600/Beige%20Business%20Brand%20Color%20Wheel%20Palette%20Facebook%20Post%20(7).png"
+  },
+  {
+    title: "Land Your Dream Job: A Masterclass in Job Hunting Success",
+    description: "This immersive masterclass equips you with the winning strategies to navigate the modern job market and land the role you deserve.",
+    date: new Date(2024, 2, 28), // May 27, 2024
+    rsvpLink: "https://example.com/rsvp/coding-workshop",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhE-OXdp-Aah1C_L5hKlk8KQHHSFHNHEZuoQEk9Rb5LEJncc0rgVXeX-AwLP0U7Zr79pIQdDShltnjWQghbojCb0ThjGFsv_J9CFsQc-KsqCQEmrSYf3g-FR0jMKjSrTWXykYIhrOuJNLQjYLK1yeBobKxyMnhZbRKjsaGmpvUX7xc3Xuk/s1600/Beige%20Business%20Brand%20Color%20Wheel%20Palette%20Facebook%20Post%20(7).png"
+  },
+  {
+    title: "Beyond the Chat: Supercharge Your Productivity with ChatGPT",
+    description: "Learn the basics of HTML, CSS, and JavaScript in this interactive workshop.",
+    date: new Date(2024, 3, 6), // May 27, 2024
+    rsvpLink: "https://example.com/rsvp/coding-workshop",
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhE-OXdp-Aah1C_L5hKlk8KQHHSFHNHEZuoQEk9Rb5LEJncc0rgVXeX-AwLP0U7Zr79pIQdDShltnjWQghbojCb0ThjGFsv_J9CFsQc-KsqCQEmrSYf3g-FR0jMKjSrTWXykYIhrOuJNLQjYLK1yeBobKxyMnhZbRKjsaGmpvUX7xc3Xuk/s1600/Beige%20Business%20Brand%20Color%20Wheel%20Palette%20Facebook%20Post%20(7).png"
+  },
+];
 
 addEventListener("DOMContentLoaded", (ev) => {
 
   const sponsorContainer = document.getElementById("sponsor_list")
-  function getSponsors(){
-    if (sponsorContainer != null){
-      sponsorDic.forEach((event)=>{
+  function getSponsors() {
+    if (sponsorContainer != null) {
+      sponsorDic.forEach((event) => {
         const sponsorElement = document.createElement("div")
         sponsorElement.classList.add("sponsor")
-        sponsorElement.innerHTML=`
+        sponsorElement.innerHTML = `
           <img src="${event.img}">
           <p>${event.description}</p>
         `
 
         sponsorContainer.appendChild(sponsorElement)
-        
+
       })
     }
   }
@@ -105,17 +110,31 @@ addEventListener("DOMContentLoaded", (ev) => {
 
   const perkContainer = document.getElementById("perks")
 
+
+  function isSingleDigit(input) {
+    // console.log(input)
+    if (input < 10) {
+      return `0${input}`
+    }
+    return input
+  }
+
   function getPerks() {
+    let count = 1
     if (perkContainer != null) {
       perkDic.forEach((event) => {
         const perkCard = document.createElement("div");
         perkCard.classList.add("card");
         perkCard.classList.add("event-description");
-
+        count = isSingleDigit(count)
         perkCard.innerHTML = `
         <img src=${event.img}>
+        <div>
+        <h1>${count}</h1>
         <h3><a href="">${event.title}</a></h3>
+        </div>
        `;
+        count++
 
         perkContainer.appendChild(perkCard)
 
@@ -212,10 +231,10 @@ addEventListener("DOMContentLoaded", (ev) => {
     button.textContent = i;
     button.addEventListener("click", () => {
       let nextButton = document.getElementById("nextBtn")
-      if (currentPage < numPages && nextButton.style.visibility == "hidden"){
-        nextButton.style.visibility="visible"
-      }else{
-        nextButton.style.visibility="hidden"
+      if (currentPage < numPages && nextButton.style.visibility == "hidden") {
+        nextButton.style.visibility = "visible"
+      } else {
+        nextButton.style.visibility = "hidden"
       }
       currentPage = i;
       displayPage(i);
@@ -233,16 +252,16 @@ addEventListener("DOMContentLoaded", (ev) => {
     nextButton.addEventListener("click", () => {
       currentPage++;
       displayPage(currentPage, numPages);
-      if (currentPage >= numPages){
-        nextButton.style.visibility ="hidden"
+      if (currentPage >= numPages) {
+        nextButton.style.visibility = "hidden"
       }
     });
-  
+
     backButton.addEventListener("click", () => {
       currentPage--;
       displayPage(currentPage);
-      if (currentPage <= numPages[0]){
-        backButton.style.display ="none"
+      if (currentPage <= numPages[0]) {
+        backButton.style.display = "none"
       }
     });
     eventsPagination.appendChild(nextButton);
